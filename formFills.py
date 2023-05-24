253f05c6fb23efeb86e44cd9360d03685dafb605
Jesusgozt <72524429+Jesusgozt@users.noreply.github.com>
2023-05-23 23:16:59 -0500

    formFills.py

---
+++ formFills.py
@@ -0,0 +1,55 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, EqualTo, ValidationError
+from passlib.hash import pbkdf2_sha256
+from models import User
+
+
+def invalid_credentials(form, field):
+    """Username and password validation """
+
+    username_entered = form.username.data
+    password_entered = field.data
+
+    # Check valid user
+
+    user_object = User.query.filter_by(username=username_entered).first()
+    if user_object is None:
+        raise ValidationError("Username or password is incorrect")
+    elif not pbkdf2_sha256.verify(password_entered, user_object.password):
+        raise ValidationError("Username or password is incorrect")
+
+
+class RegistrationForm(FlaskForm):
+    """ Registration form """
+
+    username = StringField('username_label',
+                           validators=[InputRequired(message="Username required"),
+                                       Length(min=4, max=25, message="Username must be between 4-25 characters")])
+
+    password = PasswordField('password_label',
+                             validators=[InputRequired(message="Password required"),
+                                         Length(min=4, max=25, message="Password must be between 4-25 characters")])
+
+    confirm_pswd = PasswordField('confirm_password_label',
+                                 validators=[InputRequired(message="Password required"),
+                                             EqualTo('password', message="Passwords must match")])
+
+    submit_button = SubmitField('Create')
+
+    def validate_username(self, username):
+        user_object = User.query.filter_by(username=username.data).first()
+        if user_object:
+            raise ValidationError("Username already exists")
+
+
+class LoginForm(FlaskForm):
+    """ Login Form """
+
+    username = StringField('username_label', validators=[
+                           InputRequired(message="Username required")])
+
+    password = PasswordField('password_label', validators=[InputRequired(
+        message="Password required"), invalid_credentials])
+
+    submit_button = SubmitField('Login')
